13c1ec2fabfe5519369d5dbdfe35258043c892bf
Richard Melchior <raustin.melchior@gmail.com>
2018-06-21 00:52:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/' do
 
-    @analyzed_text = TextAnalyzer.new(params.to_s)
+    @analyzed_text = TextAnalyzer.new(params.keys.join.to_s)
     binding.pry
     erb :results
   end
